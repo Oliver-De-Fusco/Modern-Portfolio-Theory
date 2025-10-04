9160ec9b0aae07f14ec4e95f4d554d1b803a14ae
Oliver <oliverd@live.co.uk>
2025-10-04 15:11:43 +0100

    cleaning

--- lib.py
+++ lib.py
@@ -1,16 +1,11 @@
 import numpy as np
-import pandas as pd
-import typing
-
-PORTFOLIO_LIMIT = 100_000
-RISK_FREE = 0
 
 # Generate portfolios
 def create_portfolio(mean_returns: np.array, weights: list[float], co_variance_table: np.array) -> list[float, float]:
     """
     Calculates portfolio returns and variance based on weights, covariance is pre-calculated to reduce computation.
 
-    Return, std
+    output -> returns, std
     """
 
     expected_return = np.sum(weights * mean_returns)
